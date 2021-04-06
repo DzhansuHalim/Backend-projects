8985eae4526389fee64946aefa19d06d6468ad5e
Dzhansu <dzhansu.halim@student.odisee.be>
2021-04-06 13:32:15 +0200

    Update

---
+++ Course02/Course02/CustomerManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Course02
+{
+    public class CustomerManager
+    {
+        public void GetCustomer(Customer[] customers)
+        { 
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("- Name: " + customer.Name + "(" + customer.Age + ")");
+
+            }
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            Console.WriteLine($"\n Added Customer: {customer.Name}({customer.Age}) ");
+        }
+
+        public void DeleteCustomer(Customer customer)
+        {
+            Console.WriteLine($"\n Deleted Customer: {customer.Name}({customer.Age}) ");
+        }
+
+    }
+}
--- Course02/Course02/Program.cs
+++ Course02/Course02/Program.cs
@@ -6,7 +6,29 @@ namespace Course02
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Customer customer1 = new Customer();
+            customer1.Name = "Tomy";
+            customer1.Age = 22;
+            customer1.Email = "tomy@gmail.com";
+
+            Customer customer2 = new Customer();
+            customer2.Name = "Coni";
+            customer2.Age = 30;
+            customer2.Email = "coni@gmail.com";
+
+            Customer customer3 = new Customer();
+            customer3.Name = "Derik";
+            customer3.Age = 28;
+            customer3.Email = "derik@gmail.com";
+
+            Customer[] customers = new Customer[] { customer1, customer2};
+            CustomerManager customerManager = new CustomerManager();
+             
+            Console.WriteLine("=== The customer accounts ===");
+            customerManager.GetCustomer(customers);
+            customerManager.AddCustomer(customer3);
+            customerManager.DeleteCustomer(customer1);
+
         }
     }
 }
